a8d76f817d057af3cf805b494f2bc9a031810f55
T01320 <diego.rangel@compasso.com.br>
2022-03-16 14:52:36 -0300

    chore: change expect baseUrl

--- cypress/integration/select.spec.js
+++ cypress/integration/select.spec.js
@@ -4,10 +4,10 @@
 describe('Select', () => {  
   beforeEach(() => {
       cy.visit('/?path=/story/components-web-atoms-select--select-base');
-      cy.url().should('contain', 'components-web-atoms-select--select-base');
+      cy.url().should('contain', '/?path=/story/components-web-atoms-select--select-base');
   });
   
-  it('Search for a value inside the dropdown test', () => {
+  it('Search for a value inside the dropdown', () => {
       cy.iframe().find('.e1oqxcck2').should('be.visible').type('quis ut nam');
       cy.iframe().find('.e13ar8800').should('be.visible').contains('quis ut nam')
       cy.iframe().find('div[data-testid="div_MenuItem"] > .e13ar8800').should('be.visible').contains('quis ut nam')
